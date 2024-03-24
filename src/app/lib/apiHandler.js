4bd20d5066aa0d2efbbfb4184fca3ebd2eb59f09
Carlos Andreo <c.andreo@uma.es>
2024-03-24 19:48:49 +0100

    Fix fetchs and try catch

--- src/app/lib/apiHandler.js
+++ src/app/lib/apiHandler.js
@@ -1,70 +1,83 @@
-// Fetching
-// TODO: Check try/catch
 export async function loginUser(username, password) {
     const authString = `${username}:${password}`;
     const encodedAuth = Buffer.from(authString).toString('base64');
     const basicAuthHeader = `Basic ${encodedAuth}`;
   
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_TNLCM_BACKEND}/tnlcm/user/login`, {
-            method: 'POST',
-            headers: {
-                'Authorization': basicAuthHeader,
-                'Content-Type': 'application/json'
-            }
-        });
-  
-        if (!response.ok) {
-            throw new Error('Error in authentication');
+    const fetchLoginUser = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_TNLCM_BACKEND}/tnlcm/user/login`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': basicAuthHeader,
+                    'Content-Type': 'application/json'
+                }
+            });
+            return response
+        } catch (error) {
+            throw new Error('Failed to fetch data' + error);
         }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw new Error('Network error: ' + error.message);
+    };
+    
+    const response = await fetchLoginUser();
+    const data = await response.json();
+    const code_error = response.status;
+    if (!response.ok) {
+        const { message } = data;
+        throw new Error(message + '. \nError code: ' + code_error);
     }
+    return data;
 };
 
-export async function registerUser(username, password, email) {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_TNLCM_BACKEND}/tnlcm/user`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password, email })
-        });
+export async function registerUser(username, password, email, org) {
 
-        if (!response.ok) {
-            throw new Error('Error when registering the user');
+    const fetchRegisterUser = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_TNLCM_BACKEND}/tnlcm/user`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password, email, org })
+            });
+            return response
+        } catch (error) {
+            throw new Error('Failed to fetch data' + error);
         }
+    };
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching user data:', error.message);
-        throw error;
+    const response = await fetchRegisterUser();
+    const data = await response.json();
+    const code_error = response.status;
+    if (!response.ok) {
+        const { message } = data;
+        throw new Error(message + '. \nError code: ' + code_error);
     }
+    return data;
 };
 
 export async function getTrialNetworks(token) {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_TNLCM_BACKEND}/tnlcm/trial_networks/`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            }
-        });
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch user data');
+    const fetchTrialNetworks = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_TNLCM_BACKEND}/tnlcm/trial_networks/`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            return response
+        } catch (error) {
+            throw new Error('Failed to fetch data' + error);
         }
+    };
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching user data:', error.message);
-        throw error;
+    const response = await fetchTrialNetworks();
+    const data = await response.json();
+    const code_error = response.status;
+    if (!response.ok) {
+        const { message } = data;
+        throw new Error(message + '. \nError code: ' + code_error);
     }
+    return data;
 };
