85bb1034efe3e5d7817c9baa0c6e40d34d1e1524
Carlos Andreo <c.andreo@uma.es>
2024-03-24 09:57:24 +0100

    Fix import

--- src/app/tnlcm/logout/page.js
+++ src/app/tnlcm/logout/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { clearAuthTokens } from '@/app/lib/jwtHandler';
+import { clearAuthTokens } from '../../lib/jwtHandler';
 
 export default function LogoutPage() {
     const router = useRouter();
