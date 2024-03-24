a9908f8335e23a682a1bd55afef62d9dc744968c
Carlos Andreo <c.andreo@uma.es>
2024-03-24 19:49:02 +0100

    Add the register component and style

--- src/app/tnlcm/register/page.js
+++ src/app/tnlcm/register/page.js
@@ -2,47 +2,74 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import { registerUser } from '../../lib/apiHandler';
+import styles from './Register.module.css';
 
 export default function RegisterPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [org, setOrg] = useState('');
   const router = useRouter();
 
   const handleRegister = async (e) => {
     e.preventDefault();
+    if (!username || !password || !email || !org) {
+      alert('All fields are required');
+    }
     try {
-      await registerUser(username, password, email);
+      await registerUser(username, password, email, org);
       router.push('/tnlcm/login');
     } catch (error) {
-      console.error('Error:', error.message);
+      alert(error);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleRegister(e);
     }
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleRegister}>
-        <input
+    <div className={styles['register-container']}>
+      <form onSubmit={handleRegister} className={styles['register-form']}>
+        <h1 className={styles['register-title']}>Register in TNLCM</h1>
+        <Input
           type="text"
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKeyPress}
+          className="input-login-register"
         />
-        <input
+        <Input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          onKeyDown={handleKeyPress}
+          className="input-login-register"
         />
-        <input
+        <Input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          onKeyDown={handleKeyPress}
+          className="input-login-register"
         />
-        <button type="submit">Register</button>
+        <Input
+          type="org"
+          placeholder="Organization"
+          value={org}
+          onChange={(e) => setOrg(e.target.value)}
+          onKeyDown={handleKeyPress}
+          className="input-login-register"
+        />
+        <Button type="submit" className="button-login-register">Register</Button>
       </form>
     </div>
   );
