560d9a0aa4566d4a6633a8bcdb4964f2482fa67b
Carlos Andreo <c.andreo@uma.es>
2024-03-24 19:49:19 +0100

    Add image project

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <div>
+      <Image src="/logo6gsandbox.png" alt="Project Logo" width={500} height={300}/>
       <h1>Welcome to TNLCM frontend</h1>
       <Link href="/tnlcm/login">
         <p>Log in</p>
