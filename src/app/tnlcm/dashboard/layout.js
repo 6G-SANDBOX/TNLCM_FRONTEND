38a7231f202905343f135f8c56683db7696daf70
Carlos Andreo <c.andreo@uma.es>
2024-03-25 12:03:37 +0100

    Add layout and page to dashboard

---
+++ src/app/tnlcm/dashboard/layout.js
@@ -0,0 +1,15 @@
+import NavLink from "../../components/NavLink";
+import styles from './Dashboard.module.css';
+
+export default function Layout ({ children }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.navLinkContainer}>
+                <NavLink />
+            </div>
+            <div className={styles.childrenContainer}>
+                {children}
+            </div>
+        </div>
+    );
+};
