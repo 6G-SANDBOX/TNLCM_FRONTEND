f522b6c2e832ca6e5ac9863f30cbf267fcaf9cc9
Carlos Andreo <c.andreo@uma.es>
2024-04-08 23:50:38 +0200

    Add change password routes

--- src/components/elements/RequireAuth.js
+++ src/components/elements/RequireAuth.js
@@ -8,16 +8,19 @@ export default function RequireAuth({ children }) {
     const router = useRouter();
     
     const isRegisterPage = usePathname() === "/tnlcm/register";
+    const isVerificationRegisterPage = usePathname() === "/tnlcm/register/verification";
+    const isResetVerificationPage = usePathname() === "/tnlcm/register/change-password/reset-verification";
+    const isChangePasswordPage = usePathname() === "/tnlcm/register/change-password";
     const isMainPage = usePathname() === "/";
 
+    const isAuth = async () => {
+        const accessToken = await getAccessTokenFromLocalStorage();
+        if (!isMainPage && !isRegisterPage && !isVerificationRegisterPage && !isChangePasswordPage && !isResetVerificationPage && !accessToken) {
+            router.push("/tnlcm/login");
+        }
+    };
+    
     useEffect(() => {
-        const isAuth = async () => {
-            const accessToken = await getAccessTokenFromLocalStorage();
-            if (!isMainPage && !isRegisterPage && !accessToken) {
-                router.push("/tnlcm/login");
-            }
-        };
-
         isAuth();
     }, []);
 
