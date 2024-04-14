c7df31ae0fec663625d3d50c328fa638878e5771
Carlos Andreo <c.andreo@uma.es>
2024-04-15 00:17:51 +0200

    Add link to the image to redirect to the project web site

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div>
-      <Image src="/logo6gsandbox.png" alt="Project Logo" width={500} height={300}/>
-      <h1>Welcome to TNLCM frontend</h1>
+      <Link href="https://6g-sandbox.eu/" target="_blank">
+        <Image src="/logo6gsandbox.png" alt="Project Logo" width={500} height={300} priority />
+      </Link>
+      <h1>Trial Network Life Cycle Manager Web Portal</h1>
       <Link href="/tnlcm/login">
         <p>Log in</p>
       </Link>
