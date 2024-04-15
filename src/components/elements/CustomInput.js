b4284161610ad9c57e1e16114c6ca77b93ebba37
Carlos Andreo <c.andreo@uma.es>
2024-04-15 12:24:06 +0200

    Add field extraContent

--- src/components/elements/CustomInput.js
+++ src/components/elements/CustomInput.js
@@ -1,15 +1,20 @@
 import styles from "@/components/modules/CustomInput.module.css";
 
-export default function CustomInput({ type, placeholder, value, onChange, onKeyDown, className, required }) {
+export default function CustomInput({ type, extraContent, placeholder, value, onChange, onKeyDown, className, required }) {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-      className={`${styles[className]}`}
-      required={required}
-    />
+    <div>
+      {extraContent && (
+        <h5>{extraContent}</h5>
+      )}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        className={`${styles[className]}`}
+        required={required}
+      />
+    </div>
   );
 };
