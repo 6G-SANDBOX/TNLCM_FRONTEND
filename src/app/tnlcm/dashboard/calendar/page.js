7bf7429d9925b5311c676178ee857e55e33a2e65
Carlos Andreo <c.andreo@uma.es>
2024-04-19 17:41:50 +0200

    Add calendar component

--- src/app/tnlcm/dashboard/calendar/page.js
+++ src/app/tnlcm/dashboard/calendar/page.js
@@ -1,5 +1,18 @@
+"use client"
+
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import dayjs from "dayjs";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import styles from "./Calendar.module.css";
+
 export default function CalendarPage() {
+    const localizer = dayjsLocalizer(dayjs);
+
     return (
-        <h1>Calendar</h1>
+        <div className={styles["calendar-container"]}>
+            <Calendar
+                localizer={localizer}
+            />
+        </div>
     )
 }
