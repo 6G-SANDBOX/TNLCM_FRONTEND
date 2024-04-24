0088b099581f5f923affd3967161131582c60e68
Carlos Andreo <c.andreo@uma.es>
2024-04-24 13:21:07 +0200

    Fix loading

--- src/hooks/useTrialNetworks.js
+++ src/hooks/useTrialNetworks.js
@@ -4,9 +4,10 @@ import { getAccessTokenFromLocalStorage } from "@/lib/jwtHandler";
 
 export default function useTrialNetworks() {
     const [trialNetworks, setTrialNetworks] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const handleTrialNetworks = async (e) => {
+        setLoading(true);
         try {
             const accessToken = await getAccessTokenFromLocalStorage();
             const response = await getTrialNetworks(accessToken);
