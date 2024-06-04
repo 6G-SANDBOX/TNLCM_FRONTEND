7565ed5496062732dec0eae13737ab7a06c1c4a9
Carlos Andreo <c.andreo@uma.es>
2024-06-04 10:03:36 +0200

    Add select type

--- src/components/elements/CustomInput.js
+++ src/components/elements/CustomInput.js
@@ -1,20 +1,35 @@
 import styles from "@/components/modules/CustomInput.module.css";
 
-export default function CustomInput({ type, title, placeholder, value, onChange, onKeyDown, className, required }) {
+export default function CustomInput({ type, title, placeholder, value, options, onChange, onKeyDown, className, required }) {
   return (
     <div>
       {title && (
         <h5>{title}</h5>
       )}
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        className={`${styles[className]}`}
-        required={required}
-      />
+      {type === 'select' ? (
+        <select
+          value={value}
+          onChange={onChange}
+          className={`${styles[className]}`}
+          required={required}
+        >
+          {options.map((option, idx) => (
+            <option key={idx} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          className={`${styles[className]}`}
+          required={required}
+        />
+      )}
     </div>
   );
 };
