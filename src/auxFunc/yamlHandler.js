25622f01126a6db277a721611c13cde34935c553
Juanjo <100770302+JuanjoRC8@users.noreply.github.com>
2025-02-21 11:22:00 +0100

    Update yamlHandler.js

--- src/auxFunc/yamlHandler.js
+++ src/auxFunc/yamlHandler.js
@@ -3,13 +3,18 @@ const convertJsonToYaml = (json) => {
     let yamlString = "trial_network:\n\n"; // First line and empty line
 
     json.components.forEach(component => {
+        //Full component label also with the name if it exists becouse the component is allowed to have one
         const name = component.data.name? component.label+"-"+component.data.name : component.label;
         yamlString += `  ${name}:\n`
+        //Only the label of the component
         yamlString += `    type: ${component.label}\n`
+        //Only the name if allowed
         if (component.data.name) {
             yamlString += `    name: ${component.data.name}\n`;
         }
+        //Dependencies of the component
         yamlString += `    dependencies: []\n`
+        //Input of the component from the form data
         yamlString += `    input: {}\n`;
         
     });
